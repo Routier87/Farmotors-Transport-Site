447a269efacd06fdfc7997ca6bd8106f576c5e9d
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-03-06 16:37:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
 function generer(){
 
-let client=document.getElementById('client').value
-let trajet=document.getElementById('trajet').value
-let marchandise=document.getElementById('marchandise').value
-let prix=document.getElementById('prix').value
+let depart=document.getElementById('depart')?.value
+let arrivee=document.getElementById('arrivee')?.value
+let cargo=document.getElementById('cargo')?.value
+let date=document.getElementById('date')?.value
 
+if(document.getElementById('resultat')){
 document.getElementById('resultat').innerHTML=`
-<h3>DEVIS - FarmOtor's Transport</h3>
-<p><b>Client:</b> ${client}</p>
-<p><b>Trajet:</b> ${trajet}</p>
-<p><b>Marchandise:</b> ${marchandise}</p>
-<p><b>Prix:</b> ${prix}</p>
+<h3>PROGRAMME DE CONVOI</h3>
+<p><b>Départ:</b> ${depart}</p>
+<p><b>Arrivée:</b> ${arrivee}</p>
+<p><b>Marchandise:</b> ${cargo}</p>
+<p><b>Date:</b> ${date}</p>
+<p><b>Entreprise:</b> FarmOtor's Transport</p>
 `
+}
 }
